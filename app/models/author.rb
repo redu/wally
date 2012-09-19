6e1270df4ce28748eb705ab7e11d20a10d319d9f
fltiago <fltiago@gmail.com>
2012-09-19 11:31:00 -0300

    Modificando o modelo de author para guardar as urls necessárias.

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,16 +5,9 @@ class Author
   field :name, type: String
   field :login, type: String
   field :thumbnails, type: Hash
+  field :perfil_url, type: String
+  field :api_url, type: String
 
   embeds_one :role
   embedded_in :entry
-
-  def home_url
-    "http://www.redu.com.br/pesssoas/#{self.login}"
-  end
-
-  def api_url
-    "http://www.redu.com.br/api/users/#{self.login}"
-  end
-
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,6 +5,8 @@ require "./app/models/author"
 describe Author do
   it { should have_field(:name).of_type(String) }
   it { should have_field(:login).of_type(String) }
+  it { should have_field(:perfil_url).of_type(String) }
+  it { should have_field(:api_url).of_type(String) }
   it { should have_field(:thumbnails).of_type(Hash) }
   it { should embed_one(:role) }
   it { should be_embedded_in(:entry) }
