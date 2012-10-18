acde710dd25b777145ea1a6f9f5a1ca3dd6e7dbc
fltiago <fltiago@gmail.com>
2012-10-18 13:43:48 -0300

    Adicionando cors ao goliath.

--- server.rb
+++ server.rb
@@ -1,9 +1,24 @@
 require 'bundler/setup'
 require 'goliath'
 require 'grape'
+require "rack/cors"
+
 Dir["./app/**/*.rb"].each { |f| require f }
 
 class Application < Goliath::API
+  use Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', :headers => :any, :methods => [:get, :post,
+                                                   :options, :delete]
+    end
+
+    allow do
+      origins '*'
+      resource '/public/*', :headers => :any, :methods => :get
+    end
+  end
+
   def response(env)
     ::Wally.call(env)
   end
