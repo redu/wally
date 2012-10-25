ecec8c75edf31551e543af54886f0231d06e1665
fltiago <fltiago@gmail.com>
2012-10-25 14:58:54 -0300

    Adição de after_update para caso do author.

--- observers/author_observer.rb
+++ observers/author_observer.rb
@@ -3,11 +3,7 @@ class AuthorObserver < Untied::Consumer::Observer
 
   def after_create(model)
     author = Author.new
-    author.user_id = model["user"]["id"]
-    author.name = model["user"]["first_name"] + " " +
-      model["user"]["last_name"]
-    author.login = model["user"]["login"]
-    author.token = model["user"]["single_access_token"]
+    author = fill_params(author, model)
     Wall.create(:resource_id => "core:user_#{author.user_id}_home")
     Wall.create(:resource_id => "core:user_#{author.user_id}")
     author.save
@@ -15,5 +11,17 @@ class AuthorObserver < Untied::Consumer::Observer
 
   def after_update(model)
     author = Author.find_by(user_id: model["user"]["id"])
+    author = fill_params(author, model)
+    author.save
+  end
+
+  protected
+  def fill_params(author, model)
+    author.user_id = model["user"]["id"]
+    author.name = model["user"]["first_name"] + " " +
+      model["user"]["last_name"]
+    author.login = model["user"]["login"]
+    author.token = model["user"]["single_access_token"]
+    author
   end
 end
